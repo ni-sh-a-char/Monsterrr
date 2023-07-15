122f883fa58eda6baef2ce7b89b4d084d6882d0e
PIYUSH MISHRA <65788721+PIYUSH-MISHRA-00@users.noreply.github.com>
2023-07-16 00:06:58 +0530

    Update Monsterrr.py

--- Monsterrr.py
+++ Monsterrr.py
@@ -1,19 +1,13 @@
-from flask import Flask, request, jsonify
 import spacy
 import requests
-import json
-import logging
-
-app = Flask(__name__)
+from flask import Flask, request, jsonify
 
 nlp = spacy.load("en_core_web_sm")
 API_URL_UNDERSTANDING = "https://api.example.com/code/understanding"
 API_URL_GENERATION = "https://api.example.com/code/generation"
 API_URL_COMPLETION = "https://api.example.com/code/completion"
 
-# Set up logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+app = Flask(__name__)
 
 # Code understanding
 def understand_code(code):
@@ -37,11 +31,9 @@ def understand_code(code):
 
             return tokens, syntax_tree, variables, functions, understanding_results
         else:
-            logger.error(f"Code understanding API failed with status code: {response_understanding.status_code}")
             return None
 
     except requests.RequestException as e:
-        logger.error(f"Error occurred during code understanding API request: {str(e)}")
         return None
 
 # Code generation
@@ -54,11 +46,9 @@ def generate_code():
             generated_code = response.json().get("code")
             return generated_code
         else:
-            logger.error(f"Code generation API failed with status code: {response.status_code}")
             return None
 
     except requests.RequestException as e:
-        logger.error(f"Error occurred during code generation API request: {str(e)}")
         return None
 
 # Code completion
@@ -73,77 +63,44 @@ def complete_code(code, position):
             suggestions = completion_results.get("suggestions")
             return suggestions
         else:
-            logger.error(f"Code completion API failed with status code: {response.status_code}")
             return None
 
     except requests.RequestException as e:
-        logger.error(f"Error occurred during code completion API request: {str(e)}")
         return None
 
-# Validate and test the generated code
-def validate_and_test_code(code):
-    try:
-        # Execute the code in a controlled environment
-        exec_locals = {}
-        exec_globals = {}
-        exec(code, exec_globals, exec_locals)
-
-        # Perform assertions and test the code behavior
-        assert exec_locals['multiply'](2, 3) == 6
-        assert exec_locals['multiply'](5, 5) == 25
-
-        # Add more assertions and test cases as needed
-
-        # If all assertions pass, code is valid and tests pass
-        return True
-
-    except Exception as e:
-        # Handle any exceptions or failed assertions
-        logger.error(f"Code validation and testing failed: {str(e)}")
-        return False
-
-@app.route('/understand', methods=['POST'])
+@app.route("/understand", methods=["POST"])
 def handle_understand():
-    code = request.json.get('code')
+    code = request.json.get("code")
     result = understand_code(code)
     if result:
         tokens, syntax_tree, variables, functions, understanding_results = result
-        response_data = {
-            'tokens': tokens,
-            'syntax_tree': syntax_tree,
-            'variables': variables,
-            'functions': functions,
-            'understanding_results': understanding_results
-        }
-        return jsonify(response_data), 200
+        return jsonify({
+            "tokens": tokens,
+            "syntax_tree": syntax_tree,
+            "variables": variables,
+            "functions": functions,
+            "understanding_results": understanding_results
+        })
     else:
-        return jsonify({'error': 'Failed to understand code'}), 400
+        return jsonify({"error": "Failed to understand the code."}), 500
 
-@app.route('/generate', methods=['GET'])
+@app.route("/generate", methods=["GET"])
 def handle_generate():
     generated_code = generate_code()
     if generated_code:
-        return jsonify({'code': generated_code}), 200
+        return jsonify({"generated_code": generated_code})
     else:
-        return jsonify({'error': 'Failed to generate code'}), 400
+        return jsonify({"error": "Failed to generate code."}), 500
 
-@app.route('/complete', methods=['POST'])
+@app.route("/complete", methods=["POST"])
 def handle_complete():
-    code = request.json.get('code')
-    position = request.json.get('position')
+    code = request.json.get("code")
+    position = request.json.get("position")
     suggestions = complete_code(code, position)
     if suggestions:
-        return jsonify({'suggestions': suggestions}), 200
+        return jsonify({"suggestions": suggestions})
     else:
-        return jsonify({'error': 'Failed to get code completion suggestions'}), 400
+        return jsonify({"error": "Failed to complete code."}), 500
 
-@app.route('/validate', methods=['POST'])
-def handle_validate():
-    code = request.json.get('code')
-    if validate_and_test_code(code):
-        return jsonify({'message': 'Code is valid and tests pass'}), 200
-    else:
-        return jsonify({'error': 'Code is invalid or tests failed'}), 400
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
